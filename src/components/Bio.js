14c45c623a8ced4994d44671f04bfd15b261afee
Katy-fry <katyvfry@gmail.com>
2021-11-18 19:43:49 -0600

    header and bio update

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -61,15 +61,15 @@ export default function Bio() {
       <InnerDiv>
         <ImgStyle src={katypic} />
         <TextDiv>
-          <p>Hi there! My name is <EmStyled>Katy Fry</EmStyled>. I am a <EmStyled>designer</EmStyled> and <EmStyled>software developer</EmStyled> who calls Nashville, TN home. </p>
+          <p>Hi there! My name is <EmStyled>Katy Fry</EmStyled>. I am a <EmStyled>designer</EmStyled> and <EmStyled>software engineer</EmStyled> who calls Nashville, TN home. </p>
 
           <p>I have always been fascinated by computers--I taught myself Photoshop and how to make Myspace templates back in the social media&apos;s heyday.</p>
 
           <p>I moved to Nashville from the Missouri Ozarks in 2010 to pursue a degree in Theology and English and began working as an Editor for a nonprofit publisher. After 8 years, I transitioned into the roll of Communications Director at a large church. This reignited my passion for graphic design, but I knew I wanted something more.</p>
 
-          <p>Enter: Nashville Software School. I began the Part-Time Web Development program in January 2021. I quickly fell in love with code and how it allowed me to be both analytical and creative--uniting both my love of science and of art.</p>
+          <p>Enter: Nashville Software School. I began the Part-Time Web Development program in January 2021. I quickly fell in love with code and how it allowed me to be both analytical and creative--uniting both my love of science and of art. I am now a contract software engineer who has worked on applications used by millions of people per month. I am so honored to get to do what I do. </p>
 
-          <p>As a developer, I try to make everything I do both <EmStyled>functional</EmStyled> and <EmStyled>beautiful</EmStyled>.</p>
+          <p>As a developer, I try to make everything I do both <EmStyled>functional</EmStyled> and <EmStyled>beautiful</EmStyled>. And foster that love in others as well!</p>
         </TextDiv>
       </InnerDiv>
     </BioDiv>
